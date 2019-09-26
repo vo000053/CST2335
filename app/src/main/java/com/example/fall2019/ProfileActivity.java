428fb196db7ec59a65ee5db2ee9588591947dee6
vo000053 <vo000053@algonquinlive.com>
2019-09-26 16:19:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fall2019/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.fall2019;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+
+
+public class ProfileActivity extends MainActivity{
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+
+    }
+/*
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+*/
+
+
+}
